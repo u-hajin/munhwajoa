0db930e3080b9182201610d1cbb95bdfe54089c4
YooSuYeon <usuyn000@gmail.com>
2022-05-23 17:14:15 +0900

    design: 기본 UI 구성

---
+++ app/src/main/java/com/doremifa/munhwajoa/FavoriteAdapter.kt
@@ -0,0 +1,10 @@
+package com.doremifa.munhwajoa
+
+import com.doremifa.munhwajoa.database.Event
+
+class FavoriteAdapter(private val values: List<Event>) {
+
+
+
+
+}
--- app/src/main/java/com/doremifa/munhwajoa/MainActivity.kt
+++ app/src/main/java/com/doremifa/munhwajoa/MainActivity.kt
@@ -2,10 +2,31 @@ package com.doremifa.munhwajoa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.doremifa.munhwajoa.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val textArray = arrayListOf("홈", "즐겨찾기")
+    val iconArray = arrayListOf(R.drawable.ic_baseline_home_24, R.drawable.ic_baseline_star_24)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = textArray[position]
+            tab.setIcon(iconArray[position])
+        }.attach()
+    }
+
+    private fun read(a: String) {
+
     }
 }
