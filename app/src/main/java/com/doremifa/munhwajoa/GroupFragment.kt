a111d76e484e477e16a654475ec72b9798f0fe6d
YooSuYeon <usuyn000@gmail.com>
2022-05-24 01:11:52 +0900

    기본 구성 (#3)

--- app/src/main/java/com/doremifa/munhwajoa/GroupFragment.kt
+++ app/src/main/java/com/doremifa/munhwajoa/GroupFragment.kt
@@ -2,18 +2,36 @@ package com.doremifa.munhwajoa
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.doremifa.munhwajoa.database.Event
 
 class GroupFragment : Fragment() {
+
+    private var columnCount = 1
+    private var eventList: ArrayList<Event> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false)
+        val view = inflater.inflate(R.layout.fragment_group_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = GroupRecyclerViewAdapter(eventList)
+            }
+        }
+        return view
     }
 
 }
