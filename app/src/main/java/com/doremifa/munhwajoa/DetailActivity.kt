3a18b4d7ce91565ccb6455b9e8dd6b2ed2e93026
zzezzee <99472980+zzezzee@users.noreply.github.com>
2022-05-30 01:33:47 +0900

    디테일에 정보 띄우기

--- app/src/main/java/com/doremifa/munhwajoa/DetailActivity.kt
+++ app/src/main/java/com/doremifa/munhwajoa/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.doremifa.munhwajoa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.doremifa.munhwajoa.database.Event
 import com.doremifa.munhwajoa.databinding.ActivityDetailBinding
 
@@ -22,7 +23,18 @@ class DetailActivity : AppCompatActivity() {
     private fun showEventDetail() {
         binding.apply {
             favoriteToggle.isChecked = event.favorite
+            Glide.with(eventImage)
+                .load(event.image)
+                .override(400,400)
+                .fitCenter()
+                .into(eventImage)
             title.text = event.title
+            date.text = event.date
+            place.text = event.place
+            target.text = event.target
+            fee.text = event.fee
+            player.text = event.player
+            program.text = event.program
         }
     }
 }
